c399f894b879713c80b4d28ff840e20df09acdcb
Ken Vilar <kenvilar@gmail.com>
2019-01-29 20:51:06 +0800

    API mod

---
+++ src/updates/api/views.py
@@ -0,0 +1,29 @@
+from updates.models import Update as UpdateModel
+
+
+class UpdateModelDetailAPI():
+    def get(self, request, *args, **kwargs):
+        return  # json
+
+    def post(self, request, *args, **kwargs):
+        return  # json
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    def delete(self, request, *args, **kwargs):
+        return  # json
+
+
+class UpdateModelListAPI():
+    def get(self, request, *args, **kwargs):
+        return  # json
+
+    def post(self, request, *args, **kwargs):
+        return  # json
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    def delete(self, request, *args, **kwargs):
+        return  # json
