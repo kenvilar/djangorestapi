4e31da7badeb85c69317b98240d86e803ac48b45
Ken Vilar <kenvilar@gmail.com>
2019-02-03 16:01:10 +0800

    Remove constraint CSRF

--- src/updates/api/views.py
+++ src/updates/api/views.py
@@ -1,10 +1,12 @@
+import json
 from django.views.generic import View
 from django.http import HttpResponse
 
 from updates.models import Update as UpdateModel
+from .mixins import CSRFExemptMixin
 
 
-class UpdateModelDetailAPIView(View):
+class UpdateModelDetailAPIView(CSRFExemptMixin, View):
     def get(self, request, id, *args, **kwargs):
         obj = UpdateModel.objects.get(id=id)
         json_data = obj.serialize()
@@ -20,11 +22,12 @@ class UpdateModelDetailAPIView(View):
         return HttpResponse({}, content_type='application/json')
 
 
-class UpdateModelListAPIView(View):
+class UpdateModelListAPIView(CSRFExemptMixin, View):
     def get(self, request, *args, **kwargs):
         qs = UpdateModel.objects.all()
         json_data = qs.serialize()
         return HttpResponse(json_data, content_type='application/json')
 
-    # def post(self, request, *args, **kwargs):
-    #     return HttpResponse({}, content_type='application/json')
+    def post(self, request, *args, **kwargs):
+        data = json.dumps({"message": "Unknown data"})
+        return HttpResponse({}, content_type='application/json')
