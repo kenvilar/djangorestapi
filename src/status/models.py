df70d7d6a2ec5d2e19e8cf229a91cfc877b8d6eb
Ken Vilar <kenvilar@gmail.com>
2019-03-10 14:27:49 +0800

    Update model and migration files

--- src/status/models.py
+++ src/status/models.py
@@ -6,6 +6,15 @@ def upload_status_image(instance, filename):
     return "updates/{user}/{filename}".format(user=instance.user, filename=filename)
 
 
+class StatusQuerySet(models.QuerySet):
+    pass
+
+
+class StatusManager(models.Manager):
+    def get_queryset(self):
+        return StatusQuerySet(self.model, using=self._db)
+
+
 # Create your models here.
 class Status(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -14,8 +23,14 @@ class Status(models.Model):
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    objects = StatusManager()
+
     def __str__(self):
         return str(self.content)[:50]
 
     def __unicode__(self):
         return str(self.content)[:50]
+
+    class Meta:
+        verbose_name = 'Status post'
+        verbose_name_plural = 'Status posts'
